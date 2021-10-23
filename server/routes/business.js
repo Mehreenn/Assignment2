f2dbbe2dcf49002813aeb6a68b9ae1dcfd2307e5
Mehreenn <mehro.ar.002@gmail.com>
2021-10-23 17:23:52 -0400

    commit of assignment2

---
+++ server/routes/business.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+let passport = require('passport');
+let businessController = require('../controllers/business');
+
+//helper function
+function requireAuth(req, res, next)
+{
+    //check if user logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
+
+//get route for business list page READ operation
+router.get('/', businessController.displayBusinessList);
+
+
+//get route for add page - CREATE operation
+router.get('/add', requireAuth, businessController.displayAddPage);
+
+//post route for processing add page - CREATE operation
+router.post('/add', requireAuth, businessController.processAddPage);
+
+//get route for displayingthe edit page - UPDATE operation
+router.get('/edit/:id', requireAuth, businessController.displayEditPage);
+
+//post route for processing the edit page - UPDATE operation
+router.post('/edit/:id', requireAuth, businessController.processEditPage);
+
+//get to perform deletion - DELETE operation
+router.get('/delete/:id', requireAuth, businessController.performDelete);
+
+module.exports = router;
