bf779e456cdcde322339fb01755b291a84b9cb9b
Mehreenn <mehro.ar.002@gmail.com>
2021-10-23 17:42:36 -0400

    fixed app.js

--- server/config/app.js
+++ server/config/app.js
@@ -77,6 +77,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -87,5 +88,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
